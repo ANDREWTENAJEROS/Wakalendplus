1522e378ff0762d80019730772201c708d4c9887
Andrew Tenajeros <jules.tenajeros@gmail.com>
2017-09-07 02:37:37 +0800

    improve layout 1

--- app/src/main/java/com/herokuapp/andrewtenajeros/wakalend/ClientProfileActivity.java
+++ app/src/main/java/com/herokuapp/andrewtenajeros/wakalend/ClientProfileActivity.java
@@ -68,7 +68,7 @@ public class ClientProfileActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Delete user button is under construction", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
