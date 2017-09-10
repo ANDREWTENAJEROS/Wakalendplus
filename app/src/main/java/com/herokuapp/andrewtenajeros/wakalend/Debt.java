71b7efe4d78029ac6fd66521c6622822ab8d67ed
Andrew Tenajeros <jules.tenajeros@gmail.com>
2017-09-10 11:45:55 +0800

    renew client profile layout

---
+++ app/src/main/java/com/herokuapp/andrewtenajeros/wakalend/Debt.java
@@ -0,0 +1,50 @@
+package com.herokuapp.andrewtenajeros.wakalend;
+
+/**
+ * Created by Andrew on 9/7/2017.
+ */
+
+public class Debt {
+
+    public String ID;
+    public String Loan;
+    public String Balance;
+    public String days_to_pay;
+    public String daily_collection;
+    public String collector;
+
+    public Debt(String ID, String loan, String days_to_pay) {
+        this.ID = ID;
+        Loan = loan;
+        Balance = loan;
+        this.days_to_pay = days_to_pay;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setCollector(String collector) {
+        this.collector = collector;
+    }
+
+    public String getLoan() {
+        return Loan;
+    }
+
+    public String getBalance() {
+        return Balance;
+    }
+
+    public String getDays_to_pay() {
+        return days_to_pay;
+    }
+
+    public String getDaily_collection() {
+        return daily_collection;
+    }
+
+    public String getCollector() {
+        return collector;
+    }
+}
